bcddd204f5686cedf297223573dc2f3049fff71c
bergmannlucas <lucaasberg@gmail.com>
2019-01-22 17:55:25 -0200

    remove unused variables

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -6,7 +6,7 @@ const btcConverter = './src/main.js';
 
 describe('Main CLI', () => {
   it('should return version of btc-converter', (done) => {
-    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+    exec(`${btcConverter} --version`, (err, stdout) => {
       if (err) throw err;
       expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
       done();
@@ -14,7 +14,7 @@ describe('Main CLI', () => {
   });
 
   it('should return the description when btc-converter --help', (done) => {
-    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+    exec(`${btcConverter} --help`, (err, stdout) => {
       if (err) throw err;
       expect(stdout.includes('Convert Bitcoin to any currency defined.')).to.be.true;
       done();
