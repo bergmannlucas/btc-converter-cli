0708bb75d33b0faf78b11af6579cb4093c48aa8f
bergmannlucas <lucaasberg@gmail.com>
2019-01-22 17:54:32 -0200

    add version and help

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,23 @@
 import { expect } from 'chai';
+import { exec } from 'child_process';
+import pkg from '../package.json';
 
-describe('Main', () => {
-  it('should do what...', () => {
-    expect(true).to.be.true;
+const btcConverter = './src/main.js';
+
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return the description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined.')).to.be.true;
+      done();
+    });
   });
 });
